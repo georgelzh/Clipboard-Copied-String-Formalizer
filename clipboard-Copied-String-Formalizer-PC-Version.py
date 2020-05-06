ae126b1f0c33a2d2bdb8a8f1abea1a2362cd9d7f
georgelzh <33303558+georgelzh@users.noreply.github.com>
2020-05-06 17:45:41 -0400

    update versions for mac and pc

---
+++ clipboard-Copied-String-Formalizer-PC-Version.py
@@ -0,0 +1,38 @@
+"""
+!python3.7
+clipboard-Copied-String-Formalizer.py
+Author: Zhihong LI (zhihongli@bennington.edu)
+Date: April 27, 2020
+ - it formalize the string that user copies from the web, pdf or anywhere
+else as long as it is string. Then it keeps the formalized string in the copy clipboard,
+so that the user can just paste the formalized string without doing anything. 
+The program will detect whether the user copied string or image, 
+if its image, the program continur, if it's string, then it formalize it
+"""
+
+import clipboard
+import time
+from PIL import ImageGrab
+
+print("Start formalizing str in clipboard...")
+
+# initialization for the loop comparison use
+copiedStr = "A1,2p;;[3[]0" # random string
+while True:
+	img = ImageGrab.grabclipboard()
+	if type(img) != type(None):
+		continue
+	else:
+		# if copied string changed, then formalize the str
+		# and place it in the clipboard so that user can paste it. 
+		copiedNewStr = clipboard.paste()
+		if copiedNewStr != copiedStr:
+			copiedNewStr = copiedNewStr.split('\r\n')
+			copiedNewStr = ''.join(copiedNewStr)
+			clipboard.copy(copiedNewStr)
+			time.sleep(0.4)
+
+"""
+https://pypi.org/project/clipboard/
+https://www.devdungeon.com/content/grab-image-clipboard-python-pillow
+"""
